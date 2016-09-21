54575736223ba63425ddda2571e97f91dfa84083
xiaofan <1353815633@qq.com>
2016-09-21 18:51:18 +0800

    LabeledStatements

---
+++ 06-LabeledStatements.playground/Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var board = [[String]](repeating: [String](repeating: "", count: 10), count: 5)
+board[3][5] = "x"
+
+rowLoop: for (rowIndex, cols) in board.enumerated() {
+    for (colIndex, col) in cols.enumerated() {
+        if col == "x" {
+            print("Found the theasure at row \(rowIndex) col \(colIndex)!")
+            break rowLoop
+        }
+    }
+}
+
+
+
