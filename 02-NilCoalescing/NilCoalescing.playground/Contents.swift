4b24f385ce37f0cd25660490b71e16a3a2f63105
xiaofan <1353815633@qq.com>
2016-09-21 16:25:57 +0800

    NilCoalescing

---
+++ 02-NilCoalescing/NilCoalescing.playground/Contents.swift
@@ -0,0 +1,43 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//let name: String? = "xiaofan"
+//let unwrappedName = name ?? "zhang"
+//print(unwrappedName)
+
+//let name1: String? = nil
+//let unwrappedName1 = name1 ?? "zhang"
+//print(unwrappedName1)
+//print(name1 ?? "hahahha")
+//
+//
+//func returnOptionalName() -> String? {
+//    return nil
+//}
+//
+//let returnName = returnOptionalName() ?? "zhang"
+//print(returnName)
+
+
+//let savedText: String
+//do {
+//    savedText = try String(contentsOfFile: "saved.text")
+//    print(savedText)
+//} catch {
+//    print("Failed to load saved text")
+//    savedText = "hello xiaoming"
+//}
+//
+//print(savedText)
+
+let savedText = (try? String(contentsOfFile: "saved.text")) ?? "hello xiaofan"
+print(savedText)
+
+
+
+
+
+
+
+
