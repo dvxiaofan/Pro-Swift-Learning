d8b765a559cfbd792373126037bac099383da7b7
xiaofan <1353815633@qq.com>
2016-09-21 18:31:28 +0800

    Destructuring

---
+++ 05-Destructuring.playground/Contents.swift
@@ -0,0 +1,42 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+//
+//let data = ("one", "two", "three")
+//
+//let one1 = data.0
+//
+//let (one, two, three) = data
+
+
+//func getPerson() -> (String, Int) {
+//    return ("Swift", 26)
+//}
+
+//let (name, age) = getPerson()
+//print("\(name) is \(age) years old")
+
+//let (_, age) = getPerson()
+//print("That is \(age) years old")
+
+
+var a = 10
+var b = 20
+
+//a = a ^ b
+//b = a ^ b
+//a = a ^ b
+
+(b, a) = (a, b)
+
+print(a)
+print(b)
+
+
+
+
+
+
+
+
+
