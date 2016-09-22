63af50be431730304686bbd27e1942945fc820fd
xiaofan <1353815633@qq.com>
2016-09-22 11:40:08 +0800

    DocumentationMarkup

---
+++ 08-DocumentationMarkup.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+/**
+ 描述语句
+ - Returns: 返回值
+ - Parameter album: 参数
+ - Throws: 抛出异常
+ - Precondition: 前提
+ - Complexity: 复杂性
+ - Authors: 作者
+ 
+ */
+
+
+
+func MyGreatFunction() {
+    // hello
+}
