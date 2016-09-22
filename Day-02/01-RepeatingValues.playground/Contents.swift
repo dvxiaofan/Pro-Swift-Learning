bd5c47be7bced8813af78b58fe6355323dc14107
xiaofan <1353815633@qq.com>
2016-09-22 11:53:00 +0800

    update

---
+++ Day-02/01-RepeatingValues.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+let headeing = "This is a heading"
+let underline = String(repeating: "=", count: headeing.characters.count)
+
+let equalsArray = [String](repeating: "=", count: headeing.characters.count)
+
+var board = [[String]](repeating: [String](repeating: "", count: 10), count: 10)
+
+
+
