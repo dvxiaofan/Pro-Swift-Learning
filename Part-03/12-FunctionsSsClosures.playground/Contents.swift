e08829cc85ee74f5143abb55e702fa97be6a7ea4
xiaofan <1353815633@qq.com>
2016-09-28 14:58:10 +0800

    FunctionsSsClosures

---
+++ Part-03/12-FunctionsSsClosures.playground/Contents.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//let input = "hhahahha"
+//input.contains("xiaoming")
+//
+//words.contains { (str) -> Bool in
+//    return true
+//}
+
+//let words = ["1989", "fearless", "red"]
+//let input = "hello you red"
+//words.contains(where: input.contains)
+
+let nums = [1, 3, 4, 5, 2]
+nums.reduce(0, +)
