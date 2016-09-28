05a754ba78fc289b641e72d6dd90e4f1077f06c5
xiaofan <1353815633@qq.com>
2016-09-28 16:36:25 +0800

    ErrorFundamentals

--- Part-03/16-ErrorFundamentals.playground/Contents.swift
+++ Part-03/16-ErrorFundamentals.playground/Contents.swift
@@ -2,4 +2,29 @@
 
 import UIKit
 
+enum PasswordError: Error {
+    case empty
+    case short
+    case obvious(message: String)
+}
+
+func encrypt(_ str: String, with password: String) throws -> String {
+    let encrypted = password + str + password
+    return String(encrypted.characters.reversed())
+}
+
+func testCatch() {
+    do {
+        let encrypted = try encrypt("secret", with: "u8u8u8")
+        print(encrypted)
+    } catch {
+        print("failed")
+    }
+}
+
+testCatch()
+
+
+
+
 
