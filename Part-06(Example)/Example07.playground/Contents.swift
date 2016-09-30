c62ca63da489ae155ff28ac87c4006010e4e47e8
xiaofan <1353815633@qq.com>
2016-09-30 12:21:44 +0800

    Example07

---
+++ Part-06(Example)/Example07.playground/Contents.swift
@@ -0,0 +1,37 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+extension Collection where Iterator.Element == String {
+    func averageLength() -> Double {
+        var sum = 0
+        var count = 0
+        
+        self.forEach {
+            sum += $0.characters.count
+            count += 1
+        }
+        
+        return Double(sum) / Double(count)
+    }
+}
+
+["hello", "abc"].averageLength()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
