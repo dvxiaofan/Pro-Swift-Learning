192a542c3c422194b70f36f8b38358f355d647d5
xiaofan <1353815633@qq.com>
2016-09-30 11:50:45 +0800

    Example02

--- Part-06(Example)/Example02.playground/Contents.swift
+++ Part-06(Example)/Example02.playground/Contents.swift
@@ -2,6 +2,16 @@
 
 import UIKit
 
+extension Integer {
+    func clamp(low: Self, high: Self) -> Self {
+        return min(max(self, low), high)
+    }
+}
+
+print(8.clamp(low: 4, high: 9))
+print(3.clamp(low: 4, high: 9))
+print(800.clamp(low: 4, high: 90))
+
 
 
 
