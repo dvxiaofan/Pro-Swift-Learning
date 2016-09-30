a0ca2a866cccf6a757e1f3dd3dc967c69d4dda13
xiaofan <1353815633@qq.com>
2016-09-30 11:46:19 +0800

    example01

---
+++ Part-06(Example)/Example01.playground/Contents.swift
@@ -0,0 +1,24 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+extension Integer {
+    func suqared() -> Self {
+        return self * self
+    }
+}
+
+let i: Int = 8
+print(i.suqared())
+
+let j: UInt = 8
+print(j.suqared())
+
+
+
+
+
+
+
+
+
